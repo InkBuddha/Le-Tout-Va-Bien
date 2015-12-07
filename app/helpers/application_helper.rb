c4c75bec20b15b47273d88ca1163f0dd3c8e3fc9
InkBuddha <stachowski.alexandra@hotmail.fr>
2015-12-07 17:18:21 +0100

    install bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+	def full_title(page_title = '')
+		base_title = "Hotel restaurant Le Tout Va Bien, Valence d'Agen"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
